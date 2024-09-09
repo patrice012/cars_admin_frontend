90e8de7aaf12a6d60077f6cfd734423a8b62f462
SamuelAMZ <>
2024-09-09 21:59:12 +0000

    Update: change redirection on home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 // components
 import Header from "../../components/Header/Header";
 
-import { RiRobot2Line } from "react-icons/ri";
+import { RiRobot2Line, RiSettingsLine } from "react-icons/ri";
+import { IoCarSport } from "react-icons/io5";
+import { IoMdPeople } from "react-icons/io";
 
 const Home = () => {
   return (
@@ -14,27 +16,33 @@ const Home = () => {
         {/* quick links */}
         <h3 className="quick-link-title">Quick Links</h3>
         <div className="quick-links stats-container-jd">
-          <Link to="/sites">
-            <div className="stat-jd">
+          <Link to="/items">
+            <div className="stat-jd" style={{ width:350}}>
               <div>
-                <p>Manage Your Sites</p>
-                <p className="desc">
-                  View all your sites, tweak their settings.
-                </p>
+                <p>Manage Your Car</p>
+                <p className="desc">View all your cars</p>
               </div>
-              <RiRobot2Line />
+              <IoCarSport />
             </div>
           </Link>
 
-          <Link to="/proxies">
-            <div className="stat-jd">
+          <Link to="/characteristics">
+            <div className="stat-jd" style={{ width:350}}>
               <div>
-                <p>Manage Your Proxy</p>
-                <p className="desc">
-                  View all your proxy, tweak their settings.
-                </p>
+                <p>Manage Your characteristics</p>
+                <p className="desc">View all your characteristics </p>
               </div>
-              <RiRobot2Line />
+              <RiSettingsLine />
+            </div>
+          </Link>
+
+          <Link to="/sellers">
+            <div className="stat-jd " style={{ width:350}}>
+              <div>
+                <p>Manage Your sellers</p>
+                <p className="desc">View all your sellers </p>
+              </div>
+              <IoMdPeople />
             </div>
           </Link>
         </div>
